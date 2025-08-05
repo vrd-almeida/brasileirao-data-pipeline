c76d6665d63fc548fa1939c0d4ede0547a5a54c5
vinicius <vinicius@LAPTOP-PQTPPCAK>
2025-08-05 15:14:17 -0300

    fix/query

--- db/main.py
+++ db/main.py
@@ -8,15 +8,19 @@ def execute_query() -> None:
     df_database = get_data_from_database(engine)
 
     df_csv = get_data_from_csv()
-    df_dropped = df_csv.drop(
-        ["Country", "League", "Time", "BFECH", "BFECD", "BFECA"], axis=1
-    )
-    df_dropped.to_sql("matches", engine, if_exists="replace", index=False)
 
     index_diff = set(df_csv.index.astype(int)) - set(df_database.index.astype(int))
 
     df_to_inject = df_csv.loc[list(index_diff), :]
 
-    df_to_inject.to_sql("matches", engine, if_exists="append", index=False)
+    if len(df_to_inject) > 0:
+
+        df_to_inject.to_sql("matches", engine, if_exists="append", index=True)
+        print(
+            f"✅ Data has been injected to database: {len(df_to_inject)} row injected."
+        )
+
+    else:
+        print("✅ Database was already up to date; no data injected.")
 
     return
